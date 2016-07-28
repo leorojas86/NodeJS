48a60442bd690315f7aec363856086d2524461bb
Leonardo Rojas <lrojas-as@avantica.net>
2016-07-28 13:11:54 -0600

    implementing node js service

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var http = require('http');
 var config = require('./config');
 
-var server = http.createServer(function(req, res) 
-{
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+var server = http.createServer(handleRequest);
+server.listen(config.PORT, config.HOST_NAME, onServerStarted);
 
-server.listen(config.PORT, config.HOST_NAME, function() 
+function handleRequest(req, res)
 {
-  console.log(`Server running at http://${config.HOST_NAME}:${config.PORT}/`);
-});
+	res.statusCode = 200;
+  	res.setHeader('Content-Type', 'text/plain');
+  	res.end('Hello World\n');
+}
+
+function onServerStarted()
+{
+	console.log(`Server running at http://${config.HOST_NAME}:${config.PORT}/`);
+}
